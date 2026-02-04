45421b2c5fdca55d872068b08db90f5976122d5b
bhavadharani216 <bhavadharani216@gmail.com>
2026-02-04 08:41:45 +0530

    today's programming

---
+++ input_output.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class input_output {
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name=sc.nextLine();
+        
+
+        System.out.println("Enter your age");
+        int age=sc.nextInt();
+        
+
+        System.out.println("Enter your Roll number");
+        int rollno =sc.nextInt();
+
+        System.out.println("Your Name is:"+name);
+        System.out.println("Your Age is:"+age);
+        System.out.println("Your Roll number is:"+rollno);
+
+    }
+    
+}
