7722c00d6cd472dd6f60cbacc79d34ff89ac11d8
bhavadharani216 <bhavadharani216@gmail.com>
2026-02-04 08:36:29 +0530

    today's programming

---
+++ print_multiple_line.java
@@ -0,0 +1,7 @@
+public class print_multiple_line {
+    public static void main(String[] args) {
+         System.out.println("Welcome to java ");
+        System.out.println("Learning is fun");
+        System.out.println("Practice daily");
+}
+}
