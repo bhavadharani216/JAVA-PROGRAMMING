67e0710f10e8237af05122b514d78d27fd5039c8
bhavadharani216 <bhavadharani216@gmail.com>
2026-02-04 15:36:55 +0530

    today's programming

---
+++ multiplication_table_of_2.java
@@ -0,0 +1,8 @@
+public class multiplication_table_of_2 {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            int result=2*i;
+            System.out.println("2 X "+i+" = "+result);
+        }
+    }
+}
