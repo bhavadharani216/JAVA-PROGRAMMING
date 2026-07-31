f88d37d374964b8cc079f4d85d32cdcf1eb47acf
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-07-31 11:28:08 +0530

    Create Reverse_num_3.java

---
+++ Interview Preparation/Reverse_num_3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int reverse = 0;
+        
+       do{
+
+    int digit = n % 10;
+    reverse = reverse * 10 + digit;
+    n = n / 10;
+
+}while(n != 0);
+        
+        System.out.println(reverse);
+    }
+}
