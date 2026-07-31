9a95738735006ed106c35fe63afb33aa61aa2edf
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-07-31 14:03:16 +0530

    Create Happy_Num_1.java

---
+++ Interview Preparation/Happy_Num_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        while (n != 1 && n != 4) {
+
+            int sum = 0;
+
+            while (n != 0) {
+
+                int digit = n % 10;
+
+                sum = sum + (digit * digit);
+
+                n = n / 10;
+            }
+
+            n = sum;
+        }
+
+        if (n == 1)
+            System.out.println("Happy Number");
+        else
+            System.out.println("Not Happy Number");
+    }
+}
