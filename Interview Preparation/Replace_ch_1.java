f3beda70abd6565caf5749564740ea8a3718e885
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-08-04 10:55:16 +0530

    Create Replace_ch_1.java

---
+++ Interview Preparation/Replace_ch_1.java
@@ -0,0 +1,15 @@
+//replace character using replace function
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        
+        String replace = s.replace("a","?");
+        
+       System.out.println(replace);
+  
+    }    
+}
