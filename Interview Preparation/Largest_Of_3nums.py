c4b4b0a0339c47aa7721f00a50cb1aaa37877642
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-08-04 09:55:37 +0530

    Create Largest_Of_3nums.py

---
+++ Interview Preparation/Largest_Of_3nums.py
@@ -0,0 +1,10 @@
+a= int(input())
+b=int(input())
+c=int(input())
+
+if( a > b and a >c):
+    print("a is greater", a)
+elif ( b > a and b > c):
+    print("b is greater", b)
+else:
+    print("c is greater",c)
