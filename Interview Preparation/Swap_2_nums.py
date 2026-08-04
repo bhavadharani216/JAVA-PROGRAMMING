f0ca44c67f12210e132962fd245fc5eb45189582
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-08-04 09:44:02 +0530

    Add Swap_2_nums.py for swapping two numbers

---
+++ Interview Preparation/Swap_2_nums.py
@@ -0,0 +1,10 @@
+n= int(input())
+m=int(input())
+
+print("Before swapping the value of n is ",n)
+print("Before swapping the value of m is ",m)
+temp = n
+n=m
+m=temp
+print("After swapping the value of n is ",n)
+print("After swapping the value of m is ",m)
