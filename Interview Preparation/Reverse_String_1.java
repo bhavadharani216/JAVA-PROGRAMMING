311e7356fef118158db18633305fea754b20fec4
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-08-05 14:19:59 +0530

    Add Reverse_String_1.java to reverse a string

---
+++ Interview Preparation/Reverse_String_1.java
@@ -0,0 +1,20 @@
+// method 1 - using for loop
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
